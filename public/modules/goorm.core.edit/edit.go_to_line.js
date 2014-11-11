d72e5561e99bd03f4e577f2b53f09dfc3134b815
JJayde <miniim@naver.com>
2014-11-11 14:56:48 +0900

    Bug fix - Go to line doesnt show on merge window

--- public/modules/goorm.core.edit/edit.go_to_line.js
+++ public/modules/goorm.core.edit/edit.go_to_line.js
@@ -12,93 +12,94 @@ goorm.core.edit.go_to_line = {
 	// dialog: null,
 	// buttons: null,
 	editor: null,
-	flag: false,	//jeongmin: whether keydown is binded already or not
+	flag: false, //jeongmin: whether keydown is binded already or not
 
-	success: function () {
+	success: function() {
 		var self = this;
 
-		if(this.flag == false) {	//jeongmin: if keydown isn't binded yet
-			$("#edit_toolbar_inputbox").off("keydown");	//jeongmin: remove event handler which is binded before
+		if (this.flag == false) { //jeongmin: if keydown isn't binded yet
+			$("#edit_toolbar_inputbox").off("keydown"); //jeongmin: remove event handler which is binded before
 			$("#edit_toolbar_ok").parent().off("mousedown");
-			$("#edit_toolbar_inputbox").keydown(function (e) {
+			$("#edit_toolbar_inputbox").keydown(function(e) {
 				var ev = e || event;
 
-				if (ev.keyCode == 27) {	//jeongmin: add this again for esc
+				if (ev.keyCode == 27) { //jeongmin: add this again for esc
 					// esc key
-					$(this).blur();	//jeongmin: go to line is done
-					return false;	//jeongmin: stop event
-				}
-
-				else if (ev.keyCode == 13) {
+					$(this).blur(); //jeongmin: go to line is done
+					return false; //jeongmin: stop event
+				} else if (ev.keyCode == 13) {
 					self.move();
 					e.stopPropagation();
 					e.preventDefault();
 
-					$(this).blur();	//jeongmin: go to line is done
+					$(this).blur(); //jeongmin: go to line is done
 					return false;
-				}
-
-				else if ((ev.ctrlKey || ev.metaKey) && ev.shiftKey && ev.keyCode == 76) {	//jeongmin: when edit toolbar is shown, if ctrl + shift + l (go to line shortcut) is pushed, exit
-					$(this).blur();	//jeongmin: go to line is done
+				} else if ((ev.ctrlKey || ev.metaKey) && ev.shiftKey && ev.keyCode == 76) { //jeongmin: when edit toolbar is shown, if ctrl + shift + l (go to line shortcut) is pushed, exit
+					$(this).blur(); //jeongmin: go to line is done
 					return false;
 				}
 			});
 
 			//button 
-			$("#edit_toolbar_ok").parent().on("mousedown", "#edit_toolbar_ok", function () {
+			$("#edit_toolbar_ok").parent().on("mousedown", "#edit_toolbar_ok", function() {
 				self.move();
 				$("#edit_toolbar").toggleClass('hidden');
 				return false;
-			});	
+			});
 
-			this.flag = true;	//jeongmin: keydown is binded
+			this.flag = true; //jeongmin: keydown is binded
 		}
 	},
 
-	move: function () {
-		var window_manager = core.module.layout.workspace.window_manager;
-
+	move: function() {
 		// Get current active_window's editor
-		if (window_manager.window[window_manager.active_window].editor) {
+		if (this.editor) {
 			// Get current active_window's CodeMirror editor
-			this.editor = window_manager.window[window_manager.active_window].editor.editor;
+			// this.editor = window_manager.window[window_manager.active_window].editor.editor;	// hidden by jeongmin: editor is decided at show, so no need to do it again
 			// Get input query of this dialog
 			var keyword = $("#edit_toolbar_inputbox").val();
+			var window_manager = core.module.layout.workspace.window_manager;
 			// Call search function of goorm.core.file.findReplace with keyword and editor	
 
 			//editor error fix --heeje
 			$("#edit_toolbar").toggleClass('hidden');
-			if(isNaN(keyword) || keyword <= 0) return;
+			if (isNaN(keyword) || keyword <= 0) return;
 
 			this.editor.setCursor(parseInt(keyword, 10) - 1, 0);
 
 			//scroll location calculate
-			var container_id=core.module.layout.workspace.window_manager.window[window_manager.active_window].container;
-			var to_mean =  ($('#'+container_id+' .bd').height()/ (window_manager.window[window_manager.active_window].editor.font_size+3) ) /2;
-			this.editor.scrollIntoView(   parseInt(parseInt(keyword, 10) - 1 - to_mean, 10 )  );
-			this.editor.scrollIntoView(   parseInt(parseInt(keyword, 10) - 1 + to_mean, 10 )  );
-			this.editor.scrollIntoView(  parseInt(keyword, 10) - 1   );
+			var container_id = window_manager.window[window_manager.active_window].container;
+			var to_mean = ($('#' + container_id + ' .bd').height() / (this.font_size + 3)) / 2;
+			this.editor.scrollIntoView(parseInt(parseInt(keyword, 10) - 1 - to_mean, 10));
+			this.editor.scrollIntoView(parseInt(parseInt(keyword, 10) - 1 + to_mean, 10));
+			this.editor.scrollIntoView(parseInt(keyword, 10) - 1);
 			this.editor.focus();
 
 		}
 	},
 
-	show: function () {
+	show: function() {
 
 		var window_manager = core.module.layout.workspace.window_manager;
+		var active_window = window_manager.window[window_manager.active_window];
 
 		// Get current active_window's editor
-		if (window_manager.window[window_manager.active_window] !== undefined) {
+		if (active_window !== undefined) {
 			$("#edit_toolbar_inputbox").val("");
 
 			// Get current active_window's CodeMirror editor
-			var editor = window_manager.window[window_manager.active_window].editor.editor;
-			this.editor = editor;
+			if (active_window.editor) {
+				this.editor = active_window.editor.editor;
+				this.font_size = this.editor.font_size; // jeongmin: use font size differently according to editor type
+				
+			} else {
+				return false;
+			}
 
-			return true;	//jeongmin: not error
+			return true; //jeongmin: not error
 		} else {
 			alert.show(core.module.localization.msg.alert_cannot_exist_editor);
-			return false;	//jeongmin: error
+			return false; //jeongmin: error
 		}
 	}
 };
